6973ee565f69518da464bb7ad48cb9c44b163136
vigneshxcode1 <vigneshxcode@gmail.com>
2025-08-07 22:07:43 +0530

    aug 7  1

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="about">
         <h1>about</h1>
         <h2 className="t"> <span id="welcome">Welcome to Muthus Petals,</span>  
-        <span id="sub">where nature meets beauty. Our range of natural shampoo, soap, and cosmetic products are crafted with love and care, using only the finest ingredients to nourish your skin and hair. At Muthus Petals, we infuse nature's essence into our cosmetics, crafting gentle, effective formulas that bloom with every use. Let the soft touch of petals soothe your skin, and the sweet scent of nature uplift your senses.</span></h2>
+        <span id="sub">where nature meets beauty.Our range of natural shampoo, soap, and cosmetic products are crafted with love and care, using only the finest ingredients to nourish your skin and hair. At Muthus Petals, we infuse nature's essence into our cosmetics, crafting gentle, effective formulas that bloom with every use. Let the soft touch of petals soothe your skin, and the sweet scent of nature uplift your senses.</span></h2>
         
       </div>
     </>
